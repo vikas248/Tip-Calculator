6d6fa082fa0e6b9fc0c85e059395bec0fa74b70b
Saumya154 <saumyagupta2420@gmail.com>
2021-09-30 15:24:54 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace tip_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxCharges = new System.Windows.Forms.TextBox();
+            this.textBoxPeople = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxTip = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelBox2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxCharges
+            // 
+            this.textBoxCharges.BackColor = System.Drawing.Color.DimGray;
+            this.textBoxCharges.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxCharges.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.textBoxCharges.Location = new System.Drawing.Point(291, 80);
+            this.textBoxCharges.Name = "textBoxCharges";
+            this.textBoxCharges.Size = new System.Drawing.Size(281, 34);
+            this.textBoxCharges.TabIndex = 0;
+            this.textBoxCharges.TextChanged += new System.EventHandler(this.textBoxCharges_TextChanged);
+            // 
+            // textBoxPeople
+            // 
+            this.textBoxPeople.BackColor = System.Drawing.Color.DimGray;
+            this.textBoxPeople.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxPeople.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.textBoxPeople.Location = new System.Drawing.Point(291, 180);
+            this.textBoxPeople.Name = "textBoxPeople";
+            this.textBoxPeople.Size = new System.Drawing.Size(281, 34);
+            this.textBoxPeople.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(291, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(308, 44);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tip Calculator";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.YellowGreen;
+            this.label2.Location = new System.Drawing.Point(64, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 33);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bill";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.YellowGreen;
+            this.label4.Location = new System.Drawing.Point(64, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 33);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "People";
+            // 
+            // textBoxTip
+            // 
+            this.textBoxTip.BackColor = System.Drawing.Color.DimGray;
+            this.textBoxTip.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxTip.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.textBoxTip.Location = new System.Drawing.Point(289, 129);
+            this.textBoxTip.Name = "textBoxTip";
+            this.textBoxTip.Size = new System.Drawing.Size(283, 34);
+            this.textBoxTip.TabIndex = 6;
+            this.textBoxTip.TextChanged += new System.EventHandler(this.textBoxTip_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.YellowGreen;
+            this.label3.Location = new System.Drawing.Point(64, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 33);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tip(%)";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(289, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 41);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.YellowGreen;
+            this.label5.Location = new System.Drawing.Point(428, 369);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(193, 33);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Total Tip/person";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.YellowGreen;
+            this.label6.Location = new System.Drawing.Point(64, 369);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(191, 33);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Total bill/person";
+            // 
+            // labelBox2
+            // 
+            this.labelBox2.AutoSize = true;
+            this.labelBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelBox2.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelBox2.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.labelBox2.Location = new System.Drawing.Point(261, 369);
+            this.labelBox2.Name = "labelBox2";
+            this.labelBox2.Size = new System.Drawing.Size(75, 35);
+            this.labelBox2.TabIndex = 14;
+            this.labelBox2.Text = "00.00";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label7.Location = new System.Drawing.Point(616, 369);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 35);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "00.00";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.ImageKey = "(none)";
+            this.button2.Location = new System.Drawing.Point(445, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 41);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Font = new System.Drawing.Font("Showcard Gothic", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.ForeColor = System.Drawing.Color.YellowGreen;
+            this.label8.Location = new System.Drawing.Point(277, 307);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(330, 28);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Enter Bill,Tip and People";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.labelBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxTip);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPeople);
+            this.Controls.Add(this.textBoxCharges);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tip Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxCharges;
+        private System.Windows.Forms.TextBox textBoxPeople;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxTip;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tip_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double charges, people, tip, tipperson, total;
+            
+            try
+            {
+                
+                charges = double.Parse(textBoxCharges.Text);
+                Type tp1 = textBoxCharges.Text.GetType();
+                tip = double.Parse(textBoxTip.Text);
+                Type tp2 = textBoxTip.Text.GetType();
+                people = double.Parse(textBoxPeople.Text);
+                Type tp3 = textBoxPeople.Text.GetType();
+                if (people <= 0.0 )
+                {
+                    textBoxPeople.Text = "Enter valid number >=1";
+                    textBoxPeople.ForeColor = Color.Red;
+                    label7.Text = "None";
+                    labelBox2.Text = "None";
+
+                }
+                else
+                {
+                    label8.Text = "Results are";
+                    label8.ForeColor = Color.OrangeRed;
+                    tipperson = (charges * tip / 100.0);
+                    total = (charges + tipperson) / people;
+                    label7.Text = (tipperson / people).ToString();
+                    labelBox2.Text = total.ToString();
+                    label7.Text = (tipperson / people).ToString();
+                    labelBox2.Text = total.ToString();
+                }
+
+            }
+            catch (FormatException)
+            {
+
+                //textBoxCharges.Text = "please enter only numbers";
+                //textBoxCharges.ForeColor = Color.Red;
+                textBoxCharges.Text = "0.0";
+                    textBoxPeople.Text = "0.0";
+                    textBoxTip.Text = "0.0";
+
+                label8.Text = "Reset and Enter correct data";
+                label8.ForeColor = Color.RosyBrown;
+
+
+            }
+            catch (Exception ex)
+            {
+                label7.Text = ex.GetType() + ":" + ex.Message;
+            }
+
+
+
+
+        }
+
+        private void textBoxTip_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            textBoxCharges.Text = "";
+            
+            textBoxTip.Text = "";
+            textBoxPeople.Text = "";
+            label7.Text = "0.0";
+            labelBox2.Text = "0.0";
+            label8.Text= "Enter Bill,Tip and People";
+            textBoxPeople.ForeColor = Color.Black;
+
+
+        }
+
+        private void textBoxCharges_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
